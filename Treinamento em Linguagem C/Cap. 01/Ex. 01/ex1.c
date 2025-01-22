05b69347e264ca066f05f672d3b85d3ef618a883
Isabela Santiago <isadelga2009@gmail.com>
2025-01-22 15:03:11 -0300

    Exercícios 1 e 2

---
+++ Treinamento em Linguagem C/Cap. 01/Ex. 01/ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Existem %d semanas no ano.\n", 52);
+    system("pause");
+    return 0;
+}
--- Treinamento em Linguagem C/teste/teste.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () {
-    char name [15];
-    printf("Enter name: ");
-    scanf("%s", name);
-    printf("Hello, %s", name);
-    return 0;
-}
